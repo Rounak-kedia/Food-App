37e8aa0e15de02450764fb1f895bad75c9ae80df
Rounak-kedia <rounakkumar.cs17@bmsce.ac.in>
2020-04-19 05:30:36 +0530

    Second Commit By Rounak.(Login,Regiter UI)

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.food_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText phone,pw;
+    Button login;
+    TextView Register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
